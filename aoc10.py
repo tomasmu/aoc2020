6b77b037fbe938fbfa530b4d4a884199d8d65928
Tomas Kristiansson <tomas.kson@gmail.com>
2020-12-06 02:02:58 +0100

    create templates for day 06-25

---
+++ aoc10.py
@@ -0,0 +1,15 @@
+#input
+import os
+file = os.path.basename(__file__).replace('.py', '.txt')
+input = open(file).read().splitlines()
+
+#format
+array = input
+
+#puzzle 1
+answer1 = ''
+print (answer1)
+
+#puzzle 2
+answer2 = ''
+print (answer2)
