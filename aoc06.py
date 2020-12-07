5fead7473b9d7962264c3193a2708cae8f344075
Tomas Kristiansson <tomas.kson@gmail.com>
2020-12-07 08:52:41 +0100

    day 06

--- aoc06.py
+++ aoc06.py
@@ -4,12 +4,28 @@ file = os.path.basename(__file__).replace('.py', '.txt')
 input = open(file).read().splitlines()
 
 #format
+#todo: split on \n\n instead?
 array = input
 
+answer_current = []
+answers = []
+for line in array:
+    if (line == ''):
+        answers.append(answer_current)
+        answer_current = []
+    else:
+        answer_current.append(line)
+answers.append(answer_current)
+
+answer_groups = [[list(b) for b in a] for a in answers]
+
 #puzzle 1
-answer1 = ''
+answer1 = sum(len(set().union(*(set(a) for a in g))) for g in answer_groups)
 print (answer1)
 
 #puzzle 2
-answer2 = ''
+import collections
+flatten_array = lambda arr: [x for xs in arr for x in xs]
+
+answer2 = sum(list(collections.Counter(flatten_array(g)).values()).count(len(g)) for g in answer_groups)
 print (answer2)
