405a929e3f28f9dba21c3ad0f84a45c700707787
Tomas Kristiansson <tomas.kristiansson@tretton37.com>
2020-12-08 19:17:07 +0100

    cleanup

--- aoc06.py
+++ aoc06.py
@@ -1,31 +1,21 @@
-#input
+# input
+import collections
 import os
 file = os.path.basename(__file__).replace('.py', '.txt')
-input = open(file).read().splitlines()
+input = open(file).read()
 
-#format
-#todo: split on \n\n instead?
-array = input
+# format
+array = input.split('\n\n')
 
-answer_current = []
-answers = []
-for line in array:
-    if (line == ''):
-        answers.append(answer_current)
-        answer_current = []
-    else:
-        answer_current.append(line)
-answers.append(answer_current)
-
+# puzzle 1
+answers = [line.split() for line in array]
 answer_groups = [[list(b) for b in a] for a in answers]
 
-#puzzle 1
 answer1 = sum(len(set().union(*(set(a) for a in g))) for g in answer_groups)
-print (answer1)
+print(answer1)
 
-#puzzle 2
-import collections
-flatten_array = lambda arr: [x for xs in arr for x in xs]
+# puzzle 2
+def flatten_array(arr): return [x for xs in arr for x in xs]
 
 answer2 = sum(list(collections.Counter(flatten_array(g)).values()).count(len(g)) for g in answer_groups)
-print (answer2)
+print(answer2)
