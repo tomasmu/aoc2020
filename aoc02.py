5ca80f16c4b8f065f2b79e1f22cc671a76e70479
Tomas Kristiansson <tomas.kristiansson@tretton37.com>
2020-12-08 22:04:55 +0100

    minor changes

--- aoc02.py
+++ aoc02.py
@@ -1,27 +1,27 @@
-#input
-import os
-file = os.path.basename(__file__).replace('.py', '.txt')
-input = open(file).read().splitlines()
-
-#format
-array = input
-
-#puzzle 1
 import re
-def is_valid(string):
-    [min, max, char, password] = re.split(r"-|: |\s", string)
-    [min, max] = [int(min), int(max)]
-    count = password.count(char)
-    return min <= count <= max
+import os
 
-answer1 = sum(is_valid(n) for n in array)
+# input
+file = os.path.basename(__file__).replace('.py', '.txt')
+input = open(file).read()
+
+# format
+array = input.splitlines()
+
+# puzzle 1
+def is_valid_password(string):
+    [min_max, char, password] = re.split(r":?\s", string)
+    [min, max] = map(int, min_max.split('-'))
+    return min <= password.count(char) <= max
+
+answer1 = sum(map(is_valid_password, array))
 print(answer1)
 
-#puzzle 2
-def is_valid2(string):
-    [min, max, char, password] = re.search(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)", string).groups()
-    [min, max] = [int(min), int(max)]
-    return (password[min - 1] == char) ^ (password[max - 1] == char)
+# puzzle 2
+def is_valid_password_2(string):
+    [min_max, char, password] = re.search(r"^(\d+-\d+) (\w): (\w+)", string).groups()
+    [min, max] = [int(n) - 1 for n in min_max.split('-')]
+    return (password[min] == char) ^ (password[max] == char)
 
-answer2 = sum(is_valid2(n) for n in array)
+answer2 = sum(map(is_valid_password_2, array))
 print(answer2)
