76c5961677269e4ac5eca3525a6f40d039e47ae3
Tomas Kristiansson <tomas.kristiansson@tretton37.com>
2020-12-08 22:48:50 +0100

    remove templates

--- aoc07.py
+++ aoc07.py
@@ -1,15 +1,42 @@
 #input
 import os
+import re
 file = os.path.basename(__file__).replace('.py', '.txt')
-input = open(file).read().splitlines()
+input = open(file).read()
 
 #format
-array = input
+array = input.splitlines()
+rules = dict()
+for arr in array:
+    arr = arr.strip(" .").replace(" no ", " 0 ").replace(" bags", " bag")
+    match = re.search(r"(?P<parent>.+) contain (?P<children>.*)", arr)
+    parent = match.group("parent")
+    children = match.group("children").split(", ")
+    rules[parent] = children
 
 #puzzle 1
-answer1 = ''
+def get_parents_containing(color):
+    return [parent for parent, bags in rules.items() for bag in bags if re.search(color, bag)]
+
+#todo: fix
+containing_gold = []
+def find_parents(parents):
+    for bag in parents:
+        contains = get_parents_containing(bag)
+        containing_gold.extend(contains)
+        find_parents(contains)
+
+find_parents(["shiny gold bag"])
+
+answer1 = len(set(containing_gold))
 print (answer1)
 
 #puzzle 2
-answer2 = ''
+def containing(color):
+    if color == "other bag":
+        return 0
+    count_name = [(int(count), name) for count, name in [bag.split(" ", 1) for bag in rules[color]]]
+    return sum(count + count * containing(name) for count, name in count_name)
+
+answer2 = containing("shiny gold bag")
 print (answer2)
--- aoc08.py
+++ aoc08.py
@@ -1,15 +1,63 @@
 #input
 import os
 file = os.path.basename(__file__).replace('.py', '.txt')
-input = open(file).read().splitlines()
+input = open(file).read()
 
 #format
-array = input
+array = input.splitlines()
 
 #puzzle 1
-answer1 = ''
+instructions = [line.split() for line in array]
+instructions = list(map(lambda op_arg: (op_arg[0], int(op_arg[1])), instructions))
+
+def get_value_at_loop(instructions):
+    i = 0
+    visited = set()
+    accumulator = 0
+    while i not in visited:
+        op, arg = instructions[i]
+        visited.add(i)
+        if op == 'acc':
+            accumulator += arg
+        elif op == 'jmp':
+            i += (arg - 1)
+        elif op == 'nop':
+            pass
+        i += 1
+    return accumulator
+
+answer1 = get_value_at_loop(instructions)
 print (answer1)
 
 #puzzle 2
-answer2 = ''
+def get_value(instructions):
+    i = 0
+    visited = set()
+    accumulator = 0
+    while i != len(instructions):
+        if i in visited:
+            return None
+        visited.add(i)
+        op, arg = instructions[i]
+        if op == 'acc':
+            accumulator += arg
+        elif op == 'jmp':
+            i += (arg - 1)
+        elif op == 'nop':
+            pass
+        i += 1
+    return accumulator
+
+def do_stuff(instructions):
+    translate = { 'jmp': 'nop', 'nop': 'jmp' }
+    for i in range(len(instructions)):
+        if instructions[i][0] not in translate:
+            continue
+        instructions[i] = (translate[instructions[i][0]], instructions[i][1])
+        result = get_value(instructions)
+        if result != None:
+            return result
+        instructions[i] = (translate[instructions[i][0]], instructions[i][1])
+
+answer2 = do_stuff(instructions)
 print (answer2)
