0ed90574587c946f248e02e3f020c17503f13b64
Tomas Kristiansson <tomas.kristiansson@tretton37.com>
2020-12-08 19:22:29 +0100

    update template

--- aoc15.py
+++ aoc15.py
@@ -1,15 +1,19 @@
-#input
+import collections
 import os
+import re
+
+# input
 file = os.path.basename(__file__).replace('.py', '.txt')
-input = open(file).read().splitlines()
+input = open(file).read()
 
-#format
-array = input
+# format
+array = input.splitlines()
+# array = input.split('\n\n')
 
-#puzzle 1
+# puzzle 1
 answer1 = ''
-print (answer1)
+print(answer1)
 
-#puzzle 2
+# puzzle 2
 answer2 = ''
-print (answer2)
+print(answer2)
