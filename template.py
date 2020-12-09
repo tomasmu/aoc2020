6d6c604ed0df5b0d14565c55ead4c559467d57d2
Tomas Kristiansson <tomas.kristiansson@tretton37.com>
2020-12-09 14:13:33 +0100

    handle double newline automatically

--- template.py
+++ template.py
@@ -1,20 +1,26 @@
+# imports
 import collections
 import functools
 import os
 import re
 
 # input
-file = os.path.basename(__file__).replace('.py', '.txt')
+file = os.path.basename(__file__).replace('.py', '_input.txt')
 input = open(file).read()
 
 # format
-array = input.splitlines()
-# array = input.split('\n\n')
+if re.search('\n\n', input):
+    array = input.split('\n\n')
+    #array = [arr.splitlines() for arr in input.split('\n\n')]
+else:
+    array = input.splitlines()
 
 # puzzle 1
-answer1 = ''
+
+answer1 = array
 print(answer1)
 
 # puzzle 2
+
 answer2 = ''
 print(answer2)
