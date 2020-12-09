dfb82dd0d28abe96b9938394817718d69bde97f6
Tomas Kristiansson <tomas.kristiansson@tretton37.com>
2020-12-09 14:10:30 +0100

    minor changes

--- get_puzzle_input.py
+++ get_puzzle_input.py
@@ -8,38 +8,41 @@ import requests
 #cookies.py
 from cookies import aoc_cookie
 
-#take year/day from argument or today's date
-if len(sys.argv) >= 1:
-    day = datetime.date.today().strftime("%d")
-    year = datetime.date.today().strftime("%Y")
+#take year/day from arguments or today's date
+year, month, day = datetime.date.today().strftime("%Y-%m-%d").split("-")
 if len(sys.argv) >= 2:
     day = sys.argv[1].zfill(2)
-if len(sys.argv) == 3:
+if len(sys.argv) >= 3:
     year = sys.argv[2]
+
+#todo: validate date n stuff
 
 url = f"https://adventofcode.com/{year}/day/{int(day)}/input"
 
 max_retries = 10
+backoff_seconds = 10
 current_try = 1
-while current_try <= 5:
+while current_try <= max_retries:
     print(f"{datetime.datetime.today()} * {current_try}/{max_retries} GET {url}")
     response = requests.get(url=url, cookies=aoc_cookie)
     if not response.ok:
-        print(f"{datetime.datetime.today()} * GET error {response.status_code}:\n'{response.text.strip()}'")
-        pass
+        print(f"{datetime.datetime.today()} * GET error {response.status_code}: '{response.text.strip()}'")
     else:
-        first = "\n".join(response.text.strip().splitlines()[0:10])
-        print(f"{datetime.datetime.today()} * GET OK {response.status_code}:\n{first}\n...")
+        lines = response.text.strip().splitlines()
+        print(f"{datetime.datetime.today()} * GET OK {response.status_code}: {len(lines)} lines")
         break
-    time.sleep(10)
+
     current_try += 1
+    if current_try <= max_retries:
+        print(f'will retry in {backoff_seconds} seconds')
+        time.sleep(backoff_seconds)
 else:
     print("max attempts reached, exiting")
     sys.exit()
 
 puzzle_input = response.text
-input_file = f"./aoc{day}_input.txt"
 
 #overwrite existing content
+input_file = f"./aoc{day}_input.txt"
 file = open(input_file, 'w')
 file.write(puzzle_input)
