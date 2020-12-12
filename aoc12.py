374ff890ffdf1efae793a3278a5f1bc9ba82a8ee
Tomas Kristiansson <tomas.kson@gmail.com>
2020-12-12 18:35:55 +0100

    testing numpy vectors

--- aoc12.py
+++ aoc12.py
@@ -3,6 +3,7 @@ import collections
 import functools
 import itertools
 import math
+import numpy
 import os
 import re
 
@@ -12,6 +13,7 @@ file = os.path.basename(__file__).replace('.py', '_input.txt')
 raw_input = open(file).read()
 print("input:", file)
 
+#format
 if re.search('\n\n', raw_input):
     puzzle = raw_input.split('\n\n')
 else:
@@ -20,51 +22,57 @@ else:
 puzzle = [(x[0], int(x[1:])) for x in puzzle]
 
 # puzzle 1
-move_table = { 'N': (0, 1), 'S': (0, -1), 'E': (1, 0), 'W': (-1, 0) }
-rotate_table = { 'L': -1, 'R': 1 }
+move_table = {
+    'N': numpy.array([0, 1]),
+    'S': numpy.array([0, -1]),
+    'E': numpy.array([1, 0]),
+    'W': numpy.array([-1, 0])
+}
+rotate_table = {
+    'L': -1,
+    'R': 1
+}
 def move_boaty_boat(puzzle):
-    angle_table = { 0: move_table['E'], 90: move_table['S'], 180: move_table['W'], 270: move_table['N'] }
-    #todo: look up vectors in python
-    x, y, angle = 0, 0, 0
+    angle_table = {
+        0: move_table['E'],
+        90: move_table['S'],
+        180: move_table['W'],
+        270: move_table['N']
+    }
+    coordinate = numpy.array([0, 0])
+    angle = 0
     for action, value in puzzle:
         if action in move_table:
-            dx, dy = move_table[action]
-            x += dx * value
-            y += dy * value
+            coordinate += value * move_table[action]
         if action in rotate_table:
             angle = (angle + rotate_table[action] * value) % 360
         if action == 'F':
-            dx, dy = angle_table[angle]
-            x += dx * value
-            y += dy * value
-    return abs(x) + abs(y)
+            coordinate += value * angle_table[angle]
+    return sum(abs(n) for n in coordinate)
 
 answer1 = move_boaty_boat(puzzle)
 print(answer1)
 
+# puzzle 2
 #slightly simplified math library
-def rotate_clockwise(x, y, a):
+def rotate_clockwise(coordinate, a):
     a = (a + 360) % 360
     cos_a = { 0: 1, 90: 0, 180: -1, 270: 0 }[a]
     sin_a = { 0: 0, 90: 1, 180: 0, 270: -1 }[a]
-    return (x*cos_a + y*sin_a, y*cos_a - x*sin_a)
+    rotation_cw = [[cos_a, sin_a], [-sin_a, cos_a]]
+    return rotation_cw @ coordinate
 
-# puzzle 2
 def move_boaty_waypoint(puzzle):
-    x, y = 0, 0
-    wx, wy = 10, 1
+    coordinate = numpy.array([0, 0])
+    waypoint = numpy.array([10, 1])
     for action, value in puzzle:
         if action in move_table:
-            dx, dy = [d * value for d in move_table[action]]
-            wx += dx
-            wy += dy
+            waypoint += value * move_table[action]
         elif action in rotate_table:
-            angle = rotate_table[action] * value
-            wx, wy = rotate_clockwise(wx, wy, angle)
+            waypoint = rotate_clockwise(waypoint, value * rotate_table[action])
         elif action == 'F':
-            x += wx * value
-            y += wy * value
-    return abs(x) + abs(y)
+            coordinate = coordinate + value * waypoint
+    return sum(abs(n) for n in coordinate)
 
 answer2 = move_boaty_waypoint(puzzle)
 print(answer2)
