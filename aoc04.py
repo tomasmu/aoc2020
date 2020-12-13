e6e63649e002b3ae84b184784c6f0205e99aa4ff
Tomas Kristiansson <tomas.kson@gmail.com>
2020-12-13 03:21:01 +0100

    minor changes

--- aoc04.py
+++ aoc04.py
@@ -7,11 +7,11 @@ input = open(file).read()
 array = input.split('\n\n')
 
 #puzzle 1
-passports = [" ".join(line.splitlines()).split() for line in array]
+passports = [' '.join(line.splitlines()).split() for line in array]
 
 def has_required_fields(passport):
-    required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"] #"cid"
-    return all(map(lambda req: req in " ".join(passport), required))
+    required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'] #'cid'
+    return all(map(lambda req: req in ' '.join(passport), required))
 
 answer1 = sum(map(has_required_fields, passports))
 print (answer1)
@@ -19,28 +19,28 @@ print (answer1)
 #puzzle 2
 import re
 def is_valid_height(height):
-    match = re.search(r"^(?P<value>\d{2,3})(?P<unit>cm|in)$", height)
+    match = re.search(r'^(?P<value>\d{2,3})(?P<unit>cm|in)$', height)
     if not match:
         return False
-    value, unit = int(match.group("value")), match.group("unit")
+    value, unit = int(match.group('value')), match.group('unit')
     if unit == 'cm':
-        return value >= 150 and value <= 193
+        return 150 <= value <= 193
     if unit == 'in':
-        return value >= 59 and value <= 76
+        return 59 <= value <= 76
     return False
 
 def is_valid_passport(passport):
     if not has_required_fields(passport):
         return False
-    pass_dict = dict(map(lambda x: x.split(":"), passport))
+    pass_dict = dict(map(lambda x: x.split(':'), passport))
     requirements = [
         1920 <= int(pass_dict['byr']) <= 2002,
         2010 <= int(pass_dict['iyr']) <= 2020,
         2020 <= int(pass_dict['eyr']) <= 2030,
         is_valid_height(pass_dict['hgt']),
-        bool(re.search("^#[0-9a-f]{6}$", pass_dict['hcl'])),
-        bool(re.search("^(amb|blu|brn|gry|grn|hzl|oth)$", pass_dict['ecl'])),
-        bool(re.search("^[0-9]{9}$", pass_dict['pid'])),
+        bool(re.search('^#[0-9a-f]{6}$', pass_dict['hcl'])),
+        bool(re.search('^(amb|blu|brn|gry|grn|hzl|oth)$', pass_dict['ecl'])),
+        bool(re.search('^[0-9]{9}$', pass_dict['pid'])),
     ]
     return all(requirements)
 
--- benchmark_it.py
+++ benchmark_it.py
@@ -1,33 +1,61 @@
-import time
+import functools
 
-def decorator_with_parameters(function):
-    def decorator(*args, **kwargs):
-        def func(f):
-            return function(f, *args, **kwargs)
-        return func
-    return decorator
+# def decorator_with_args(decorator_to_enhance):
+#     print("decorator_with_args(d)", "got decorator_to_enhance", decorator_to_enhance.__name__)
+#     @functools.wraps(decorator_to_enhance)
+#     def decorator_maker(*args, **kwargs):
+#         print("decorator_maker(a, k) got", "args:", *args, "kwargs:", **kwargs)
+#         def decorator_wrapper(function_to_decorate):
+#             print("decorator_wrapper(f) got", "function_to_decorate", function_to_decorate.__name__)
+#             return decorator_to_enhance(function_to_decorate, *args, **kwargs)
+#         return decorator_wrapper
+#     return decorator_maker
 
-@decorator_with_parameters
-def benchmark_it(function, iterations):
-    def func(*args, **kwargs):
-        t0 = time.time()
-        for _ in range(iterations):
-            result = function(*args, **kwargs)
-        t1 = time.time()
+# @decorator_with_args 
+# def decorated_decorator(function_to_decorate, *args, **kwargs): 
+#     print("decorated_decorator(f, a, k) got", "function_to_decorate", function_to_decorate.__name__, "args:", *args, "kwargs:", **kwargs)
+#     @functools.wraps(function_to_decorate)
+#     def function_wrapper(function_arg):
+#         print("wrapper(a) got", function_arg)
+#         return function_to_decorate(function_arg)
+#     return function_wrapper
 
-        plural_s = '' if iterations == 1 else 's'
-        print(f'{function.__name__} {iterations} time{plural_s} took {1000*(t1 - t0):.6f} ms')
-        return result
+# @decorated_decorator("art", "deco-rator")
+# def decorated_function(function_arg):
+#     print("decorated_function(a) got", function_arg)
+#     return "return = " + function_arg
 
-    return func
+def benchmark_it_arg(decorator_benchmark_it):
+    print("benchmark_it_arg(d)", "got decorator_benchmark_it", decorator_benchmark_it.__name__)
+    @functools.wraps(decorator_benchmark_it)
+    def decorator_maker(arg):
+        print("decorator_maker(a) got", "arg:", arg)
+        def decorator_wrapper(function_to_decorate):
+            #print("decorator_wrapper(f) got", "function_to_decorate", function_to_decorate.__name__)
+            return decorator_benchmark_it(function_to_decorate, arg)
+        return decorator_wrapper
+    return decorator_maker
 
-def time_it(func):
-    def f(*args):
+@benchmark_it_arg
+def benchmark_it(function_to_benchmark, iterations):
+    print("function_to_benchmark", function_to_benchmark.__name__, "iterations", iterations)
+    @functools.wraps(function_to_benchmark)
+    def function_wrapper(*function_args, **function_kwargs):
+        #print("wrapper got args", *function_args, "kwargs", **function_kwargs)
+        import time
+
         t0 = time.perf_counter_ns()
-        result = func(*args)
+        for _ in range(iterations):
+            result = function_to_benchmark(*function_args, **function_kwargs)
         t1 = time.perf_counter_ns()
 
-        print(f'{func.__name__} took {(t1 - t0)/1000:.6f} ms')
-        return result
+        ms = (t1 - t0)/1_000_000
+        number_of_times = '' if iterations == 1 else f'{iterations} times '
+        print(f'{function_to_benchmark.__name__} {number_of_times}took {ms:.4f} ms')
 
-    return f
+        return result
+    return function_wrapper
+
+def time_it(function_to_decorate):
+    #can i express this in terms of benchmark_it?
+    return None
